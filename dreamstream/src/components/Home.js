0ff5f14fa8f2e8b135ad0105f8ceccb19a11ee52
Carson Curry <carsoncurry52@gmail.com>
2019-01-08 19:44:03 -0600

    initial push

---
+++ dreamstream/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="container home">
+                <h4 className="center">Home Page</h4>
+            </div>
+        )
+    }
+}
+
+export default Home;
---
+++ dreamstream/src/components/MyDreamStream.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyDreamStream extends Component {
+    render() {
+        return (
+            <div className="container home">
+                <h4 className="center">MyDreamStream</h4>
+            </div>
+        )
+    }
+}
+
+export default MyDreamStream;
