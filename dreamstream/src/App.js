a46e0bedb967947b41617c85369674a8119a8216
Carson Curry <carsoncurry52@gmail.com>
2019-01-12 13:47:18 -0600

    making changes

--- dreamstream/src/App.js
+++ dreamstream/src/App.js
@@ -7,26 +7,6 @@ import MyDreamStream from './components/MyDreamStream';
 import Contact from './components/Contact';
 
 class App extends Component {
-  goTo(route) {
-    this.props.history.replace(`/${route}`)
-  }
-
-  login() {
-    this.props.auth.login();
-  }
-
-  logout() {
-    this.props.auth.logout();
-  }
-
-  componentDidMount() {
-    const { renewSession } = this.props.auth;
-
-    if (localStorage.getItem('isLoggedIn') === 'true') {
-      renewSession();
-    }
-  }
-
   render() {    
     return (
       <BrowserRouter>
