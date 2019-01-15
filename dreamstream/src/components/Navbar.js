d79e3d00bd1af0cf6b9e86a0d539e5fe304d7d9e
Carson Curry <carsoncurry52@gmail.com>
2019-01-15 13:53:08 -0600

    commit to switch to local

--- dreamstream/src/components/Navbar.js
+++ dreamstream/src/components/Navbar.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
 
 const Navbar = (props) => {
-    const { isAuthenticated } = this.props.auth;
-
     return (
         <nav className="nav-wrapper black lighten-3">
             <div className="container">
@@ -13,30 +11,6 @@ const Navbar = (props) => {
                     <li><NavLink to="/search">Search</NavLink></li>
                     <li><NavLink to="/mydreamstream">My DreamStream</NavLink></li>
                     <li><NavLink to="/contact">Contact</NavLink></li>
-                    {
-                        !isAuthenticated() && (
-                            <button
-                                id="qsLoginBtn"
-                                bsStyle="primary"
-                                className="btn-primary"
-                                onClick={this.login.bind(this)}
-                            >
-                                Log In
-                            </button>
-                        )
-                    }
-                    {
-                        isAuthenticated() && (
-                            <button
-                                id="qsLogoutBtn"
-                                bsStyle="primary"
-                                className="btn-primary"
-                                onClick={this.logout.bind(this)}
-                            >
-                                Log Out
-                            </button>
-                        )
-                    }
                 </ul>
             </div>
         </nav>
--- dreamstream/src/index.js
+++ dreamstream/src/index.js
@@ -4,12 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const routes = makeMainRoutes();
-
-ReactDOM.render(
-    <App />, 
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
