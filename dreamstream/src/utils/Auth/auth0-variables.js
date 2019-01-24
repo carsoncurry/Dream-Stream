b42bc8850aa906be13ba442546b1ac885ea6ad96
John Caldwell <jmcald0@gmail.com>
2019-01-24 17:56:52 -0600

    callback changed

--- dreamstream/src/utils/Auth/auth0-variables.js
+++ dreamstream/src/utils/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
     domain: 'carson-curry.auth0.com',
     clientId: 'pNzOIaIBOjdSVVPN6yPLsjfRa7A03tF4',
-    callbackUrl: 'http://localhost:3000/callback'
+    callbackUrl: 'https://fast-hollows-53838.herokuapp.com/callback'
 }
