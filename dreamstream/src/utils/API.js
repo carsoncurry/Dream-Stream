4efe4187e2f33a113d673d468b39c5a17c81dd90
John Caldwell <jmcald0@gmail.com>
2019-01-26 12:15:15 -0600

    added s

--- dreamstream/src/utils/API.js
+++ dreamstream/src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default {
   getSearches: function(q) {
-    return axios.get("http://api.tvmaze.com/singlesearch/shows?q=/search/api/tvm", { params: { q }});
+    return axios.get("https://api.tvmaze.com/singlesearch/shows?q=/search/api/tvm", { params: { q }});
   },
   getProfileSearches: function(id) {
     return axios.get("/api/search/" + id);
